b5406678081f1ac25450ba57927ae99ed5d83b39
abhishek <abhishekmishra6280178183@gmail.com.com>
2024-06-03 19:31:18 +0530

    first commit

--- verka.py
+++ verka.py
@@ -2,7 +2,8 @@ import streamlit as st
 import numpy as np
 import plotly.graph_objects as go
 
-st.title("Welcome Sir")
+
+st.markdown("<h1 style='text-align: center;'>Welcome Sir</h1>", unsafe_allow_html=True)
 a=st.number_input("Enter the Fat you want",value=0.0)
 b=st.number_input("Enter the Snf you want",value=0.0)
 result = st.number_input("Enter total amount you want to make:", value=0, format="%d", key="result")
@@ -11,7 +12,7 @@ fat2 = st.number_input("Enter Recon fat:", value=0.0, key="fat2")
 snf1 = st.number_input("Enter milk snf:", value=0.0, key="snf1")
 snf2 = st.number_input("Enter Recon snf:", value=0.0, key="snf2")
 
-# Plot the estimated values
+
 if st.button("Click Here"):
     x1 = np.linspace(10, result, 1000000)
     y1 = ((result * a) - (fat1 * x1)) / fat2
@@ -29,7 +30,7 @@ if st.button("Click Here"):
 
     st.plotly_chart(fig)
 
-    # Find the point where the estimated milk and recon fat values are closest
+    
     for i in range(len(x2)):
         if abs(x2[i] - x1[i]) < 1 and abs(y2[i] - y1[i]) < 1:
             st.write(f"Milk: {int(x1[i])}")
